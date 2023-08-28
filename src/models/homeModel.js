2026f643a6362a1baa5049243fa8722838829291
Mateus Vitor <mateusvitor01.mv@gmail.com>
2023-08-28 20:51:27 -0300

    update project

---
+++ src/models/homeModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const homeSchema = new mongoose.Schema({
+    titulo: {type: String, required: true},
+    descricao: String,
+});
+
+const homeModel = mongoose.model('Home', homeSchema);
+
+class Home {
+
+}
+module.exports = Home;
